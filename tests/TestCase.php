6aa5ec34f6b3cc27fe80e834e4119adefeaef0d4
ako <akosalman@yahoo.com>
2019-01-05 14:37:56 +0330

    Add tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,8 +14,8 @@ abstract class TestCase extends  \Orchestra\Testbench\TestCase
             'driver'   => 'mysql',
             'database' => 'shorturl',
             'host'   => '127.0.0.1',
-            'username'  => 'user',
-            'password' => 'pass'
+            'username'  => 'root',
+            'password' => 'mysql'
         ]);
         $app['config']->set('app.key', 'sF5r4kJy5HEcOEx3NWxUcYj1zLZLHxuu');
     }
@@ -38,6 +38,6 @@ abstract class TestCase extends  \Orchestra\Testbench\TestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations/');
     }
 }
--- tests/unit/TestLocalDriver.php
+++ tests/unit/TestLocalDriver.php
@@ -8,8 +8,22 @@ use http\Exception\InvalidArgumentException;
 class TestLocalDriver extends TestCase
 {
 
-	public function test_get_driver_exception ()
+	public function test_shorten ()
 	{
-		$this->assertEquals(\Shorturl::getDriver(), 'local', 'The default driver is not local');
+	    $long = "127.0.0.1/here/is/a/long/url";
+        $shortened = \Shorturl::shorten($long);
+        $this->assertNotEmpty($shortened);
+        $this->assertNotNull($shortened);
 	}
+
+	public function test_expand ()
+    {
+        $long1 = \Shorturl::expand("127.0.0.1/here/is/a/long/url");
+        $this->assertEquals("", $long1);
+    }
+
+    public function test_get_driver ()
+    {
+        $this->assertEquals(config("shorturl.drivers.default"), \Shorturl::getDriver());
+    }
 }
